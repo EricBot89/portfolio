ffa46323eed73e0feb047b210074ce1eab8548ff
EricBot89 <eloucks@gmail.com>
2019-10-27 15:02:36 -0400

    adds resume

--- src/comps/Resume.js
+++ src/comps/Resume.js
@@ -1,9 +1,33 @@
-import React from "react"
+import React from "react";
 
-const Resume = (props) => (
+const Resume = props => (
+  <div className="resume">
     <p>
-        Resume Text
+      I am a math graduate and software developer looking for roles in fullstack
+      web development or application development. I am a generalist with a broad
+      range of interests including schema design, building and maintaining
+      deployment pipelines and more mathematical topics like signal processing
+      and data analysis.
     </p>
-)
+    <h5>Technical Skills:</h5>
+    <ul>
+      <li>
+        Proficient: Javascript (client side and node.js) , Python, Rust, SQL,
+        MSSql, postgresql
+      </li>
+      <li>
+        Frameworks: Node.Js, React, Redux, Sequelize, SocketIO, express,
+        babel/webpack, materialUI, wasm-pack{" "}
+      </li>
+      <li>Experienced in Agile development methodologies</li>
+    </ul>
+    <h5>Education:</h5>
+    <ul>
+      <li>Fullstack Academy Of Code | Web Development immersive | 2019</li>
+      <li>CUNY Brooklyn College | BA Mathematics | 2016-2018</li>
+      <li>-Undergraduate research in Dynamical Systems</li>
+    </ul>
+  </div>
+);
 
-export default Resume
+export default Resume;
